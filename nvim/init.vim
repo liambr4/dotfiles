50ad93df96f3d8bcadbf16885a47ac86fcca82ba
liambr4 <liam.brown@dal.ca>
2022-11-03 21:13:00 -0300

    new settings

--- nvim/init.vim
+++ nvim/init.vim
@@ -3,18 +3,28 @@ set ignorecase
 set smartcase
 
 call plug#begin()
-Plug 'morhetz/gruvbox'
-Plug 'neovim/nvim-lspconfig'
-Plug 'github/copilot.vim'
 Plug 'sbdchd/neoformat'
+Plug 'neovim/nvim-lspconfig'
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'kyazdani42/nvim-web-devicons'
 Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.0' }
+Plug 'prabirshrestha/vim-lsp'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'onsails/lspkind-nvim'
+Plug 'morhetz/gruvbox'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
 call plug#end()
 colorscheme gruvbox
-luafile ~/.config/nvim/lsp.lua
+luafile ~/.config/nvim/lua/lsp/lsp.lua
 hi Normal guibg=NONE ctermbg=NONE
-autocmd BufWritePre,TextChanged,InsertLeave *.* Neoformat
+autocmd BufWritePre,TextChanged,InsertLeave *.* Neoformat 
 nnoremap <leader>ff <cmd> Telescope find_files <cr>
+nnoremap <leader>fg <cmd>lua require('telescope.builtin').find_files{ cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] }<cr>
+set updatetime=50
+set noswapfile
+set tabstop=2
+
--- nvim/lsp.lua
+++ nvim/lsp.lua
@@ -1,4 +1,69 @@
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.gopls.setup{}
+vim.opt.completeopt={"menu", "menuone", "noselect"}
+local cmp = require("cmp")
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+	  ['<C-j>'] = cmp.mapping(function()cmp.select_next_item() end),
+	  ['<C-k>'] = cmp.mapping(function()cmp.select_prev_item() end),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+local nvim_lsp = require('lspconfig')
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+--local servers = {'tsserver', 'rust_analyzer'}
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'H', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>d',"<cmd>Telescope diagnostics<cr>", bufopts) 
+end
+
+require('lspconfig').tsserver.setup{ 
+		capabilities = capabilities,
+		on_attach = on_attach,
+}
+require('lspconfig').rust_analyzer.setup{ 
+		capabilities = capabilities,
+		on_attach = on_attach,
+}
 require('lualine').setup()
 require('telescope').setup()
+
